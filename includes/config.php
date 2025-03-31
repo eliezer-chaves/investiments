90cf0f613b60365db70d0da41e826e2de246812a
Eliezer Chaves <75528314+eliezer-chaves@users.noreply.github.com>
2025-03-31 20:20:01 -0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,4 @@
+<?php
+// includes/config.php
+define('SUPABASE_URL', 'https://ttuwpchkotalpponbjzv.supabase.co');
+define('SUPABASE_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0dXdwY2hrb3RhbHBwb25ianp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MzY3NTksImV4cCI6MjA1OTAxMjc1OX0.gL_1exIGEYLiU475i_vcm06nt39VkgX7OYauMuxWDDY'); // Project Settings > API
